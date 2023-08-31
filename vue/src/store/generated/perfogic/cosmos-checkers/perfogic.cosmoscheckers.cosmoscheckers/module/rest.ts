d42c4d7d25174dc9d0d77629a9ed7ca655cd2fc9
Shinnen <phamminhdang135@gmail.com>
2023-08-31 22:44:07 +0700

    store object 1/2

--- vue/src/store/generated/perfogic/cosmos-checkers/perfogic.cosmoscheckers.cosmoscheckers/module/rest.ts
+++ vue/src/store/generated/perfogic/cosmos-checkers/perfogic.cosmoscheckers.cosmoscheckers/module/rest.ts
@@ -14,12 +14,48 @@
  */
 export type CosmoscheckersParams = object;
 
+export interface CosmoscheckersQueryAllStoredGameResponse {
+  storedGame?: CosmoscheckersStoredGame[];
+
+  /**
+   * PageResponse is to be embedded in gRPC response messages where the
+   * corresponding request message has used PageRequest.
+   *
+   *  message SomeResponse {
+   *          repeated Bar results = 1;
+   *          PageResponse page = 2;
+   *  }
+   */
+  pagination?: V1Beta1PageResponse;
+}
+
+export interface CosmoscheckersQueryGetStoredGameResponse {
+  storedGame?: CosmoscheckersStoredGame;
+}
+
+export interface CosmoscheckersQueryGetSystemInfoResponse {
+  SystemInfo?: CosmoscheckersSystemInfo;
+}
+
 /**
  * QueryParamsResponse is response type for the Query/Params RPC method.
  */
 export interface CosmoscheckersQueryParamsResponse {
   /** params holds all the parameters of this module. */
   params?: CosmoscheckersParams;
+}
+
+export interface CosmoscheckersStoredGame {
+  index?: string;
+  board?: string;
+  turn?: string;
+  black?: string;
+  red?: string;
+}
+
+export interface CosmoscheckersSystemInfo {
+  /** @format uint64 */
+  nextId?: string;
 }
 
 export interface ProtobufAny {
@@ -31,6 +67,69 @@ export interface RpcStatus {
   code?: number;
   message?: string;
   details?: ProtobufAny[];
+}
+
+/**
+* message SomeRequest {
+         Foo some_parameter = 1;
+         PageRequest pagination = 2;
+ }
+*/
+export interface V1Beta1PageRequest {
+  /**
+   * key is a value returned in PageResponse.next_key to begin
+   * querying the next page most efficiently. Only one of offset or key
+   * should be set.
+   * @format byte
+   */
+  key?: string;
+
+  /**
+   * offset is a numeric offset that can be used when key is unavailable.
+   * It is less efficient than using key. Only one of offset or key should
+   * be set.
+   * @format uint64
+   */
+  offset?: string;
+
+  /**
+   * limit is the total number of results to be returned in the result page.
+   * If left empty it will default to a value to be set by each app.
+   * @format uint64
+   */
+  limit?: string;
+
+  /**
+   * count_total is set to true  to indicate that the result set should include
+   * a count of the total number of items available for pagination in UIs.
+   * count_total is only respected when offset is used. It is ignored when key
+   * is set.
+   */
+  count_total?: boolean;
+
+  /**
+   * reverse is set to true if results are to be returned in the descending order.
+   *
+   * Since: cosmos-sdk 0.43
+   */
+  reverse?: boolean;
+}
+
+/**
+* PageResponse is to be embedded in gRPC response messages where the
+corresponding request message has used PageRequest.
+
+ message SomeResponse {
+         repeated Bar results = 1;
+         PageResponse page = 2;
+ }
+*/
+export interface V1Beta1PageResponse {
+  /** @format byte */
+  next_key?: string;
+
+  /** @format uint64 */
+  total?: string;
 }
 
 export type QueryParamsType = Record<string | number, any>;
@@ -240,6 +339,64 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
   queryParams = (params: RequestParams = {}) =>
     this.request<CosmoscheckersQueryParamsResponse, RpcStatus>({
       path: `/perfogic/cosmos-checkers/cosmoscheckers/params`,
+      method: "GET",
+      format: "json",
+      ...params,
+    });
+
+  /**
+   * No description
+   *
+   * @tags Query
+   * @name QueryStoredGameAll
+   * @summary Queries a list of StoredGame items.
+   * @request GET:/perfogic/cosmos-checkers/cosmoscheckers/stored_game
+   */
+  queryStoredGameAll = (
+    query?: {
+      "pagination.key"?: string;
+      "pagination.offset"?: string;
+      "pagination.limit"?: string;
+      "pagination.count_total"?: boolean;
+      "pagination.reverse"?: boolean;
+    },
+    params: RequestParams = {},
+  ) =>
+    this.request<CosmoscheckersQueryAllStoredGameResponse, RpcStatus>({
+      path: `/perfogic/cosmos-checkers/cosmoscheckers/stored_game`,
+      method: "GET",
+      query: query,
+      format: "json",
+      ...params,
+    });
+
+  /**
+   * No description
+   *
+   * @tags Query
+   * @name QueryStoredGame
+   * @summary Queries a StoredGame by index.
+   * @request GET:/perfogic/cosmos-checkers/cosmoscheckers/stored_game/{index}
+   */
+  queryStoredGame = (index: string, params: RequestParams = {}) =>
+    this.request<CosmoscheckersQueryGetStoredGameResponse, RpcStatus>({
+      path: `/perfogic/cosmos-checkers/cosmoscheckers/stored_game/${index}`,
+      method: "GET",
+      format: "json",
+      ...params,
+    });
+
+  /**
+   * No description
+   *
+   * @tags Query
+   * @name QuerySystemInfo
+   * @summary Queries a SystemInfo by index.
+   * @request GET:/perfogic/cosmos-checkers/cosmoscheckers/system_info
+   */
+  querySystemInfo = (params: RequestParams = {}) =>
+    this.request<CosmoscheckersQueryGetSystemInfoResponse, RpcStatus>({
+      path: `/perfogic/cosmos-checkers/cosmoscheckers/system_info`,
       method: "GET",
       format: "json",
       ...params,
---
+++ x/cosmoscheckers/keeper/system_info_test.go
@@ -0,0 +1,38 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	keepertest "github.com/perfogic/cosmos-checkers/testutil/keeper"
+	"github.com/perfogic/cosmos-checkers/testutil/nullify"
+	"github.com/perfogic/cosmos-checkers/x/cosmoscheckers/keeper"
+	"github.com/perfogic/cosmos-checkers/x/cosmoscheckers/types"
+)
+
+func createTestSystemInfo(keeper *keeper.Keeper, ctx sdk.Context) types.SystemInfo {
+	item := types.SystemInfo{}
+	keeper.SetSystemInfo(ctx, item)
+	return item
+}
+
+func TestSystemInfoGet(t *testing.T) {
+	keeper, ctx := keepertest.CosmoscheckersKeeper(t)
+	item := createTestSystemInfo(keeper, ctx)
+	rst, found := keeper.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.Equal(t,
+		nullify.Fill(&item),
+		nullify.Fill(&rst),
+	)
+}
+
+func TestSystemInfoRemove(t *testing.T) {
+	keeper, ctx := keepertest.CosmoscheckersKeeper(t)
+	createTestSystemInfo(keeper, ctx)
+	keeper.RemoveSystemInfo(ctx)
+	_, found := keeper.GetSystemInfo(ctx)
+	require.False(t, found)
+}
